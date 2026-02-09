c1bcf660d6aab75e2421cc00b5140ee96e84564e
Joseph <joseph@myruiz.com>
2026-02-09 01:13:03 -0500

    Add files via upload

---
+++ dist/clients/tailwind.config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // IMPORTANT: This tells Tailwind to look at your HTML file for classes
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        cardinal: {
+          red: '#D92323',
+          black: '#1A1A1A',
+          gray: '#555555'
+        }
+      }
+    }
+  },
+  plugins: []
+};
